a41e44122c89258a6dae50787eee1e1da7770689
Hannah Bialik <hrb44@scarletmail.rutgers.edu>
2025-11-24 10:25:46 -0500

    stuff from canvas and train.py

---
+++ hw4_files/test1.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import mnist
+import torch
+import numpy as np
+import torchvision
+
+ 
+def test(dataloader,model):
+
+    #please implement your test code#
+    ##HERE##
+    ###########################                                                                                                                                                                               
+
+    test_accuracy=0
+
+    print("test accuracy:", test_accuracy)
+
+ 
+
+def main():
+
+    pad=torchvision.transforms.Pad(2,fill=0,padding_mode='constant')
+
+    mnist_test=mnist.MNIST(split="test",transform=pad)
+
+    test_dataloader= DataLoader(mnist_test,batch_size=1,shuffle=False)
+
+    model = torch.load("LeNet1.pth")
+
+    test(test_dataloader,model)
+
+ 
+
+if __name__=="__main__":
+
+    main()
---
+++ train.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("karnikakapoor/digits")
+print("Path to dataset files:", path)
+
+#Path to dataset files: /Users/chanabialik/.cache/kagglehub/datasets/karnikakapoor/digits/versions/4
